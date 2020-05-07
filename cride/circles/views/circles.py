8ebef49a5671e2e420406b9fd509af54332a2d48
Manuel Rodrigues <mr.1311223@gmail.com>
2020-05-07 07:23:01 -0400

    Update circles methods
    Add is admin validation for update and partial update method
    Remove delete action
    Create new membership in create method.

--- cride/circles/views/circles.py
+++ cride/circles/views/circles.py
@@ -1,19 +1,31 @@
 """ Circle views """
 
 # Django REST
-from rest_framework import viewsets
+from rest_framework import mixins, viewsets
 from rest_framework.permissions import IsAuthenticated
 
 # Serializers
 from cride.circles.serializers.circles import CircleModelSerializer
 
 # Models
-from cride.circles.models import Circle
+from cride.circles.models import Circle, Membership
 
+# Permissions
+from cride.circles.permissions import IsCircleAdmin
 
-class CircleViewSet(viewsets.ModelViewSet):
+class CircleViewSet(mixins.CreateModelMixin,
+					mixins.RetrieveModelMixin,
+					mixins.UpdateModelMixin,
+					mixins.ListModelMixin,
+					viewsets.GenericViewSet):
 	serializer_class = CircleModelSerializer
-	permission_classes = (IsAuthenticated, )
+
+	def get_permissions(self):
+		""" Assign permissions based on circle action """
+		permissions = [IsAuthenticated]
+		if self.action in ['update', 'partial_update']:
+			permissions.append(IsCircleAdmin)
+		return [permission() for permission in permissions]
 
 	def get_queryset(self):
 		""" Restrict list method to public circles """
@@ -21,3 +33,16 @@ class CircleViewSet(viewsets.ModelViewSet):
 		if self.action == 'list':
 			return queryset.filter(is_public=True)
 		return queryset
+	
+	def perform_create(self, serializer):
+		""" Assign circle admin """
+		circle = serializer.save()
+		user = self.request.user
+		profile = user.profile
+		Membership.objects.create(
+			user=user,
+			profile=profile,
+			circle=circle,
+			is_admin=True,
+			remaining_invitations=10
+		)
